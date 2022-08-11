b85680f19884241b45c9e3f7c6df1bb7e8c1efaf
Kerrshak <jack723@hotmail.co.uk>
2022-08-11 18:38:00 +0100

    added functionality to update stored truth values in tasks

--- src/_ListGenerator.js
+++ src/_ListGenerator.js
@@ -1,22 +1,20 @@
 import { useState } from "react"
-//import TaskAdder from './_TaskAdder'
 import TaskList from './_TaskList'
-import TaskCompleted from ''
+//import TaskCompleted from ''
+//import TaskAdder from './_TaskAdder'
 
 //, {task: "Go on another run", done: true}, {task: "Go on a long run", done: false}
 
-function ListGenerator() {
+const ListGenerator = () => {
+    const [listItems, setListItems] = useState([{task: "Go on a run", done: false} , {task: "Go on another run", done: true}, {task: "Go on a long run", done: false}])
 
-    
-    const [listItems, setListItem] = useState([{task: "Go on a run", done: false} /*, {task: "Go on another run", done: true}, {task: "Go on a long run", done: false} */])
-    
     // const taskCompleted = () => {
         
         // }
         
     return <main>
         <h2>Outstanding tasks</h2>
-        <TaskList tasks={listItems} callback={setListItem}/>        
+        <TaskList tasks={listItems} callback={setListItems}/>        
     </main>
 }
 
--- src/_TaskList.js
+++ src/_TaskList.js
@@ -1,18 +1,23 @@
 function TaskList({tasks, callback}) {
-    console.dir(callback)
 
-    const handleClick = () => {
-        setDone((currentTasks) => { console.log(currentTasks.done)
-            return {...currentTasks, done: false}})
-    }
+
 
     return <div>
         <ul>
             {tasks.map((task, index) => {
+                const handleClick = () => {
+                    callback(() => {
+                        return tasks.map((task, checkIndex) => {
+                            if(index === checkIndex) {
+                                return {task: task.task, done: !task.done}
+                            } else return {task: task.task, done: task.done}
+                        })})
+                }
+
                 if(task.done){
-                    return <li key={`${index}`}><input className="checkbox" type="checkbox" defaultChecked onClick={handleClick}/>{task.task}</li>
+                    return <li key={`${index}`}><input className="checkbox" type="checkbox" defaultChecked onClick={handleClick}/>{task.task}{`${task.done}`}</li>
                 }else{
-                    return <li key={`${index}`}><input className="checkbox" type="checkbox"/>{task.task} </li>
+                    return <li key={`${index}`}><input className="checkbox" type="checkbox" onClick={handleClick}/>{task.task}{`${task.done}`} </li>
                 }
             })}
         </ul>
